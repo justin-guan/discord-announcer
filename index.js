9f85af39bfe8c8dc507e2d1021e58df9bc474915
Justin <justin-guan@github.com>
2016-10-25 18:43:07 -0400

    Fixed a bug where join announcements would occur if a voiceStateUpdate occurred but it was not a user joining the channel

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const tts = require(__dirname + '/text-to-mp3.js');
+const moment = require('moment');
 
 const commandModifier = '!';
 let channel;
@@ -31,8 +32,7 @@ client.on('message', (message) => {
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   if (newMember.user.client === client && newMember.user.bot) { //Bot got moved
-    channel = newMember.voiceChannel;
-    console.log('I got moved to a new channel');
+    console.log(`Someone tried to move me at ${moment().format()}!`);
   } else if (oldMember.mute !== newMember.mute || oldMember.deafen !== newMember.deafen) {
     return; // No notification on mute and deafen
   } else if (newMember.voiceChannel !== channel) { // No voiceChannelID implies that the user has left
@@ -40,7 +40,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     tts.tts(`${oldMember.user.username} has left the channel`, `voice/leave/${oldMember.user.username}.mp3`, () => {
       channel.connection.playFile(__dirname + `/voice/leave/${oldMember.user.username}.mp3`);
     });
-  } else { // User has joined some channel
+  } else if (newMember.voiceChannel === channel){ // User has joined some channel
     console.log(`${newMember.user.username} has joined the channel`);
     tts.tts(`${newMember.user.username} has joined the channel`, `voice/join/${newMember.user.username}.mp3`, () => {
       channel.connection.playFile(__dirname + `/voice/join/${newMember.user.username}.mp3`);
