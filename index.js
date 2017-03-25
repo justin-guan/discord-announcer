997046f451f63cde83bf054af86311e3017af265
justin-guan <dengitzjg@gmail.com>
2017-03-25 19:53:13 -0400

    Converted console logging to winston logging

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment');
+const Promise = require('bluebird');
 const tts = require(__dirname + '/text-to-mp3.js');
+const LOGGER = require(__dirname + '/logger.js');
 
 const commandModifier = '!';
 let channel, banished = true;
@@ -17,11 +20,16 @@ if (!fs.existsSync(__dirname + '/voice/join')) {
 if (!fs.existsSync(__dirname + '/voice/leave')) {
   fs.mkdirSync(__dirname + '/voice/leave');
 }
+if (!fs.existsSync(__dirname + '/logs')) {
+  fs.mkdirSync(__dirname + '/logs');
+}
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN).then(()=> {
+  LOGGER.info(`${moment().format()}: Client login success`);
+});
 
 client.on('ready', () => {
-  console.log('Ready!');
+  LOGGER.info(`${moment().format()}: Client ready`);
 });
 
 client.on('message', (message) => {
@@ -38,13 +46,15 @@ client.on('message', (message) => {
         (msg.member.voiceChannel).join()
           .then(connection => {
             banished = false;
-            console.log(`Connected: ${connection.ready}`);
+            LOGGER.info(`${moment().format()}: Joined ${msg.member.voiceChannel.name}`);
             channel = connection.channel;
             tts.tts(`Hello World!`, `voice/hello.mp3`, () => {
               connection.playFile(__dirname + '/voice/hello.mp3');
             });
           })
-          .catch(console.log);
+          .catch((err) => {
+            LOGGER.error(`${moment().format()}: ${err}`);
+          });
       });
   } else if (message.content.startsWith(commandModifier + 'banish')) {
     message.delete()
@@ -68,12 +78,12 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   if (newMember.user.id === client.user.id && oldMember.voiceChannelID !== newMember.voiceChannelID) { // Bot voiceStateUpdate
     process.exit(1); // No support for moving a bot to another channel without banishing and resummoning
   } else if (oldMember.voiceChannelID !== channel.id && newMember.voiceChannelID === channel.id) {
-    console.log(`${newMember.user.username} has joined the channel`);
+    LOGGER.info(`${moment().format()}: ${newMember.user.username} has joined the channel`);
     tts.tts(`${newMember.user.username} has joined the channel`, `voice/join/${newMember.user.username}.mp3`, () => {
       channel.connection.playFile(__dirname + `/voice/join/${newMember.user.username}.mp3`);
     });
   } else if (oldMember.voiceChannelID === channel.id && newMember.voiceChannelID !== channel.id) {
-    console.log(`${oldMember.user.username} has left the channel`);
+    LOGGER.info(`${moment().format()}: ${oldMember.user.username} has left the channel`);
     tts.tts(`${oldMember.user.username} has left the channel`, `voice/leave/${oldMember.user.username}.mp3`, () => {
       channel.connection.playFile(__dirname + `/voice/leave/${oldMember.user.username}.mp3`);
     });
---
+++ logger.js
@@ -0,0 +1,26 @@
+'use strict';
+const winston = require('winston');
+winston.emitErrs = true;
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.File({
+      level: 'info',
+      filename: __dirname + '/logs/logs.log',
+      handleExceptions: true,
+      json: false,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false
+    }),
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    })
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
