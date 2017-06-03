54f6da6287e19b88ceb69b9688b07ae74e03fb04
justin-guan <dengitzjg@gmail.com>
2017-06-02 22:53:06 -0400

    Refactor join & leave logic

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ process.title = 'Discord Announcer';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Promise = require('bluebird');
-const voicesynth = Promise.promisifyAll(require(__dirname + '/voicesynth.js'));
+//const voicesynth = Promise.promisifyAll(require(__dirname + '/voicesynth.js'));
+const voicesynth = require(__dirname + '/voicesynth.js');
 const LOGGER = require(__dirname + '/logger.js');
 const config = require(__dirname + '/../config/config.js');
 
 const commands = new Map();
+const sayJoinAsync = Promise.promisify(sayJoin);
+const sayLeaveAsync = Promise.promisify(sayLeave);
 
 (function init() {
   commands.set(config.get('command.trigger') + 'help', showHelp);
+  commands.set(config.get('command.trigger') + 'test', test);
   client.login(config.get('discord.token'))
     .then(LOGGER.info('Client login success'))
     .catch(LOGGER.error);
@@ -32,23 +36,48 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   if (!newMember.voiceChannel || newMember.id === client.user.id) {
     return;
   }
-  say(newMember, true);
+  sayJoinAsync(newMember).then(() => {
+    sayLeaveAsync(oldMember);
+  });
 });
 
-function say(member, join) {
+function sayJoin(member, callback) {
   let name = member.nickname ? member.nickname : member.user.username;
-  member.voiceChannel.join().then(() => {
-    if (join) {
-      console.log((member.voiceChannel.connection));
-      voicesynth.synthAsync(`${name} joined the channel`,
+  member.voiceChannel.join().then((connection) => {
+    voicesynth.synth(`${name} joined the channel`,
         __dirname + `/../voice/join/${name}.mp3`)
-        .then();
-    } else {
-      voicesynth.synthAsync(`${name} left the channel`,
-        __dirname + `/../voice/leave/${name}.mp3`);
-      member.voiceChannel.connection.playFile(
-        __dirname + `/../voice/leave/${name}.mp3`);
-    }
+      .then(() => {
+        connection.playFile(
+          __dirname + `/../voice/join/${name}.mp3`);
+      })
+      .then(() => {
+        connection.disconnect();
+      })
+      .then(() => {
+        if (typeof callback === 'function') {
+          callback();
+        }
+      });
+  });
+}
+
+function sayLeave(member, callback) {
+  let name = member.nickname ? member.nickname : member.user.username;
+  member.voiceChannel.join().then((connection) => {
+    voicesynth.synth(`${name} left the channel`,
+        __dirname + `/../voice/leave/${name}.mp3`)
+      .then(() => {
+        connection.playFile(
+          __dirname + `/../voice/leave/${name}.mp3`);
+      })
+      .then(() => {
+        connection.disconnect();
+      })
+      .then(() => {
+        if (typeof callback === 'function') {
+          callback();
+        }
+      });
   });
 }
 
@@ -98,20 +127,38 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   }
 });
 */
-process.on('SIGTERM', () => {
-  cleanUp();
-});
-
-process.on('SIGINT', () => {
-  cleanUp();
-});
-
-process.on('SIGQUIT', () => {
-  cleanUp();
-});
+process.on('SIGTERM', cleanUp);
+process.on('SIGINT', cleanUp);
+process.on('SIGQUIT', cleanUp);
 
 function showHelp(message) {
   message.reply("Help documentation coming soon...");
+}
+
+function test(message) {
+  message.delete()
+    .then(msg => {
+      if (!msg.member.voiceChannel) {
+        msg.reply('You need to be in a voice channel to summon me!');
+        return;
+      }
+      (msg.member.voiceChannel).join()
+        .then(connection => {
+          LOGGER.info(`Joined ${msg.member.voiceChannel.name}`);
+          connection.playFile(__dirname + '/../voice/hello.mp3');
+          /*
+          voicesynth.synth(`Hello World!`, __dirname + `/../voice/hello.mp3`, () => {
+            connection.playFile(__dirname + '/../voice/hello.mp3');
+          });
+          */
+        })
+        .catch((err) => {
+          LOGGER.error(`${err}`);
+        });
+    })
+    .catch((err) => {
+      LOGGER.warn(`${err}`);
+    });
 }
 
 function cleanUp() {
--- src/voicesynth.js
+++ src/voicesynth.js
@@ -7,18 +7,18 @@ const LOGGER = require(__dirname + '/logger.js');
 const config = require(__dirname + '/../config/config.js');
 
 const synth = function(text, path) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     const writable = fs.createWriteStream(path);
-    writable.on('error', function(err) {
+    writable.on('error', (err) => {
       reject(err);
+    });
+    writable.on('finish', () => {
+      resolve();
     });
     request
       .get(config.get('voiceRSS.url') + text)
       .on('error', LOGGER.error)
-      .pipe(writable)
-      .on('finish', function() {
-        resolve();
-      });
+      .pipe(writable);
   });
 };
 
