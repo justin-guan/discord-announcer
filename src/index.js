2f3b29afe1bf87fc8f3ed6590a9959c3150db175
justin-guan <dengitzjg@gmail.com>
2017-06-04 23:48:17 -0400

    Removed commented code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ process.title = 'Discord Announcer';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Promise = require('bluebird');
-//const voicesynth = Promise.promisifyAll(require(__dirname + '/voicesynth.js'));
 const voicesynth = require(__dirname + '/voicesynth.js');
 const LOGGER = require(__dirname + '/logger.js');
 const config = require(__dirname + '/../config/config.js');
@@ -50,7 +49,7 @@ function sayJoin(member, callback) {
     voicesynth.synth(`${name} joined the channel`,
         __dirname + `/../voice/join/${name}.mp3`)
       .then(() => {
-        connection.playFile(
+        return connection.playFile(
           __dirname + `/../voice/join/${name}.mp3`);
       })
       .then(() => {
@@ -84,52 +83,6 @@ function sayLeave(member, callback) {
   });
 }
 
-/*
-function summon(message) {
-  message.delete()
-    .then(msg => {
-      if (!msg.member.voiceChannel) {
-        msg.reply('You need to be in a voice channel to summon me!');
-        return;
-      }
-      (msg.member.voiceChannel).join()
-        .then(connection => {
-          banished = false;
-          LOGGER.info(`Joined ${msg.member.voiceChannel.name}`);
-          channel = connection.channel;
-          tts.tts(`Hello World!`, __dirname + `/../voice/hello.mp3`, () => {
-            connection.playFile(__dirname + '/../voice/hello.mp3');
-          });
-        })
-        .catch((err) => {
-          LOGGER.error(`${err}`);
-        });
-    })
-    .catch((err) => {
-      LOGGER.warn(`${err}`);
-    });
-}
-
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-  if (!channel || banished) { // Bot is not connected
-    return;
-  }
-  if (newMember.user.id === client.user.id && oldMember.voiceChannelID !== newMember.voiceChannelID) { // Bot voiceStateUpdate
-    LOGGER.error(`Bot was incorrectly moved. Recovering from unhandled error by exitting...`);
-    process.exit(1); // No support for moving a bot to another channel without banishing and resummoning
-  } else if (oldMember.voiceChannelID !== channel.id && newMember.voiceChannelID === channel.id) {
-    LOGGER.info(`${newMember.user.username} has joined the channel`);
-    tts.tts(`${newMember.user.username} has joined the channel`, __dirname + `/../voice/join/${newMember.user.username}.mp3`, () => {
-      channel.connection.playFile(__dirname + `/../voice/join/${newMember.user.username}.mp3`);
-    });
-  } else if (oldMember.voiceChannelID === channel.id && newMember.voiceChannelID !== channel.id) {
-    LOGGER.info(`${oldMember.user.username} has left the channel`);
-    tts.tts(`${oldMember.user.username} has left the channel`, __dirname + `/../voice/leave/${oldMember.user.username}.mp3`, () => {
-      channel.connection.playFile(__dirname + `/../voice/leave/${oldMember.user.username}.mp3`);
-    });
-  }
-});
-*/
 process.on('SIGTERM', cleanUp);
 process.on('SIGINT', cleanUp);
 process.on('SIGQUIT', cleanUp);
@@ -149,11 +102,9 @@ function test(message) {
         .then(connection => {
           LOGGER.info(`Joined ${msg.member.voiceChannel.name}`);
           connection.playFile(__dirname + '/../voice/hello.mp3');
-          /*
-          voicesynth.synth(`Hello World!`, __dirname + `/../voice/hello.mp3`, () => {
-            connection.playFile(__dirname + '/../voice/hello.mp3');
-          });
-          */
+        })
+        .then(connection => {
+          connection.disconnect();
         })
         .catch((err) => {
           LOGGER.error(`${err}`);
