c6c9ed9f1944179850a06f370a3f20823ccc2f50
justin-guan <dengitzjg@gmail.com>
2017-06-07 00:15:02 -0400

    Refer to github for documentation

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function showHelp(info) {
-  info.message.reply("Help documentation coming soon...");
+  info.message.reply("See documentation here: https://github.com/justin-guan/discord-announcer#commands");
 }
 
 exports.showHelp = showHelp;
